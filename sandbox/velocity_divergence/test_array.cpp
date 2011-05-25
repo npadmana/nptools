2e90df09aaa1efda6f5dfe81d8069967a9adae5d
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-05-25 16:22:13 -0400

    Even cooler tests.

--- sandbox/velocity_divergence/test_array.cpp
+++ sandbox/velocity_divergence/test_array.cpp
@@ -19,12 +19,21 @@ template <class T> void index_test2(double &x, T& ilist) {
     x = ilist[0] + ilist[1] +10;
 }
 
+
+typedef array<i_, 3> itype;
+
+void add_arr(double &x, itype ilist, array3d& b) {
+    x += b(ilist);
+}
+
+
 int main() {
 
     cout << "Hello world\n";
 
     // Define a 3d array
     array3d a1(extents[3][3][3]);
+    array3d b1(extents[3][3][3]);
 
     // Do a simple fill
     cout << "Filling array with 10s\n";
@@ -57,6 +66,27 @@ int main() {
     multi_for_indices(a2, index_test2<boost::array<i_, 2> >);
     multi_for(a1, cout << _1 << " ");
     cout << endl;
+
+    cout << "--------------\n";
+
+    // Fill a1, and b1
+    ii = 0.0;
+    multi_for(a1, _1 = var(ii)++);
+    multi_for(b1, _1 = var(ii)++);
+    // Print these out
+    multi_for(a1, cout << _1 << " ");
+    cout << endl;
+    multi_for(b1, cout << _1 << " ");
+    cout << endl;
+
+    // Now do something complicated with indices
+    multi_for_native_indices(a1, bind(add_arr, _1, _2, var(b1)));
+    // Print these out
+    multi_for(a1, cout << _1 << " ");
+    cout << endl;
+    multi_for(b1, cout << _1 << " ");
+    cout << endl;
+
 
 
 
