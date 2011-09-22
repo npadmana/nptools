2404bb46ee332a3fdb07e8a1101fb26c65d1b207
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-09-21 20:43:16 -0400

    Started a simple code to do BAO fisher stuff for Bob

---
+++ modules/wfirst_fisher/bao_test_for_cahn.cpp
@@ -0,0 +1,67 @@
+// Some code for tests that Bob Cahn would like for the BAO data
+// Nikhil Padmanabhan, Yale
+// Sitting at LBL, Sept 2011
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <tclap/CmdLine.h>
+#include <eigen3/Eigen/Dense>
+#include <boost/format.hpp>
+#include <boost/foreach.hpp>
+#include <cosmoutils.h>
+#include <npio.h>
+#include "wfirst_detf.h"
+#include "bao_forecast.h"
+// We will need some more
+
+using namespace std;
+using namespace Eigen;
+
+int main(int argc, char** argv) {
+
+   // Read in the command line
+   TCLAP::CmdLine cmd("Generate an RSD fisher matrix",' ', "0.0");
+   TCLAP::ValueArg<double> area("a", "area", "area of sky [deg^2]", true, 1.e4, "area of sky");
+   TCLAP::ValueArg<double> sigma8("s", "sigma8", "sigma8 ", false, 0.8, "sigma8 [0.8]");
+   TCLAP::ValueArg<double> bias0("b", "bias", "bias at z=0", false, 1.0, "bias [1.0]");
+   TCLAP::ValueArg<double> dz("z", "dz", "delta z [0.1]", false, 0.1, "delta z");
+   TCLAP::UnlabeledValueArg<string> infn("infn", "BAO input file (zmin, zmax, dn/dzdA)", true, "","BAO input file (zmin, zmax, dn/dzdA)");
+   TCLAP::UnlabeledValueArg<string> outfn("outfn", "BAO fisher file", true, "","BAO fisher filename");
+   cmd.add(area);
+   cmd.add(sigma8);
+   cmd.add(bias0);
+   cmd.add(dz);
+   cmd.add(infn);
+   cmd.add(outfn);
+   cmd.parse(argc, argv);
+
+   // Define the basic cosmology
+   detf fidcosmo = detf_fiducial();
+   double D0 = growth(1.0, fidcosmo);
+
+
+   // Read in the file
+   typedef boost::tuple<double, double> myrec;
+   vector<myrec> ll = readAsciiFile(infn.getValue(), &TupleAdaptor<myrec, 2>);
+   cout << ll.size() << " lines read in ....." << endl;
+   double s8 = sigma8.getValue();
+   cout << "Using sigma8 = " << s8 << endl;
+   cout << "Using area = " << area.getValue() << endl;
+   cout << "Using dz = " << dz.getValue() << endl;
+   cout << "Using bias(z=0) = " << bias0.getValue() << endl;
+
+   double zmin, zmax, zmid, num, errD, errH, bb, beta, amid;
+   Matrix2d cov;
+   cout << "#zmid num bias beta errD(%) errH(%) \n";
+   BOOST_FOREACH( myrec l1, ll) {
+        zmid = l1.get<0>(); num = l1.get<1>(); amid = z2a(zmid);
+        bb = bias0.getValue() * D0/growth(amid, fidcosmo);
+        beta = fgrowth(amid, fidcosmo)/bb;
+        zmin = zmid - dz.getValue()/2.0; zmax = zmid + dz.getValue()/2.0;
+        cov = bao_forecast_shell(num, s8*bb, 0.0, beta, zmin, zmax, area.getValue(), 0.5, true);
+        errD = sqrt(cov(0,0)); errH = sqrt(cov(1,1));
+        cov = cov/1.e4;
+        cout << boost::format("%1$4.2f %2$6.1f %3$4.2f %4$5.3f %5$4.3f %6$4.3f\n") % zmid % num % bb % beta % errD % errH;
+   }
+}
