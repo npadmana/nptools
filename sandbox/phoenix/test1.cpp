581bb1f1c6a41a403020ca7e334afea784005a15
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2011-09-29 16:18:33 -0400

    More playing around

--- sandbox/phoenix/test1.cpp
+++ sandbox/phoenix/test1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <boost/phoenix.hpp>
 
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using namespace boost::phoenix;
+
+template <typename F>
+void print(F f) {
+    cout << f() << endl;
+}
+
+int main() {
+    using boost::phoenix::arg_names::arg1;
+
+
+    cout << "Welcome to Phoenix \n";
+    print(val(5));
+    int i=10;
+    print(ref(i));
+
+    // Example
+    // Allocate a vector
+    vector<int> v(10, 0);
+    // Now print it out
+    for_each(v.begin(), v.end(), cout << arg1 << " ");
+    cout << endl;
+
+    // Now set to 1 to 10
+    i = 0;
+    for_each(v.begin(), v.end(), arg1=(ref(i)++));
+    for_each(v.begin(), v.end(), cout << arg1 << " ");
+    cout << endl;
+
+
+
+}
